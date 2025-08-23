878250454e31e2661f9a478f87ac16f3c404de30
Mehmet Şükrü Kavak <msukrukavak@gmail.com>
2025-08-24 01:26:50 +0300

    Initial Commit

---
+++ BookTracking/BookTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackingApp.swift
+//  BookTracking
+//
+//  Created by Mehmet Şükrü Kavak on 24.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
